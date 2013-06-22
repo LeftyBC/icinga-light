3488eaa6e4120be8ac2d034ee65e078fd3192047
Colin Moller <colin@unixarmy.com>
2013-06-21 18:38:18 -0700

    Add support for my custom-wired cylon notifier using beagleboard GPIO pins

--- icinga.py
+++ icinga.py
@@ -22,6 +22,14 @@ off = "off"
 
 current_color = "#666666"
 
+leds = {
+        "service_crit": "P9.12",
+        "service_warn": "P9.14",
+        "service_unkn": "P9.15",
+        "host_crit"   : "P9.16",
+        "host_warn"   : "P9.21",
+        "host_unkn"   : "P9.11"
+}
 
 def debug(msg):
     global debug
@@ -37,16 +45,24 @@ def set_light(color="off", count=1):
                                        (off, color))
     current_color = color
 
+def tell_cylon(check_type, check_count):
+	global leds
+	if check_type in leds:
+		if check_count is 0:
+			digitalWrite(leds[check_type], LOW)
+		else:
+			for i in range(1,check_count):
+				digitalWrite(leds[check_type], LOW)
+				delay(100)
+				digitalWrite(leds[check_type], HIGH)
 
 def poll_icinga():
-    f = open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                          "config.yaml"))
-    config = yaml.load(f)
 
-    url = config["icinga_baseurl"] + "/cgi-bin/tac.cgi?jsonoutput"
-    username = config["username"] or None
-    password = config["password"] or None
-    interval = config["interval"] or 30
+    if cylon_mode:
+	from mrbbio import *
+	for led in leds:
+		pinUnexport(digitalPinDef[leds[led]])
+		pinMode(leds[led], OUTPUT)
 
     # build request, auth'd if necessary
     request = urllib2.Request(url)
@@ -67,44 +83,78 @@ def poll_icinga():
             host_pct = jvars["percent_host_health"]
             service_pct = jvars["percent_service_health"]
 
-            services_warning = jvars["services_warning"]
-            services_warning_unack = jvars["services_warning_unacknowledged"]
-            services_critical = jvars["services_critical"]
+            services_warning = int(jvars["services_warning"])
+            services_warning_unack = int(jvars["services_warning_unacknowledged"])
+            services_critical = int(jvars["services_critical"])
             services_critical_unack = jvars["services_critical_unacknowledged"]
-            services_unknown = jvars["services_unknown"]
-            services_unknown_unack = jvars["services_unknown_unacknowledged"]
+            services_unknown = int(jvars["services_unknown"])
+            services_unknown_unack = int(jvars["services_unknown_unacknowledged"])
 
-            hosts_down_unack = jvars["hosts_down_unacknowledged"]
+            hosts_down_unack = int(jvars["hosts_down_unacknowledged"])
+            hosts_warn_unack = int(jvars["hosts_unreachable_unacknowledged"])
+            hosts_unknown_unack = int(jvars["hosts_pending"])
+		
+	    if cylon_mode:
+	            tell_cylon("service_crit", services_critical_unack)
+		    tell_cylon("service_warn", services_warning_unack)
+		    tell_cylon("service_unkn", services_unknown_unack)
 
-            if services_critical_unack > 0:
-                debug("crit srv: " + str(services_critical_unack))
-                set_light(color=red, count=services_critical_unack)
-            elif services_warning_unack > 0:
-                debug("warn srv: " + str(services_warning_unack))
-                set_light(color=orange, count=services_warning_unack)
-            elif hosts_down_unack > 0:
-                debug("hosts down: " + str(hosts_down_unack))
-                set_light(color=blue, count=hosts_down_unack)
-            elif services_unknown_unack > 0:
-                debug("services unknown: " + str(services_unknown_unack))
-                set_light(color=purple, count=services_unknown_unack)
-            else:
-                debug("all is well")
-                if current_color != darkgreen:
-                    set_light(color=darkgreen)
+		    tell_cylon("host_crit", hosts_down_unack)
+		    tell_cylon("host_warn", hosts_warn_unack)
+		    tell_cylon("host_unkn", hosts_unknown_unack)
+		
+	    else:
+		    # assume we're using the notifier instead
+
+		    if services_critical_unack > 0:
+			debug("crit srv: " + str(services_critical_unack))
+			set_light(color=red, count=services_critical_unack)
+		    elif services_warning_unack > 0:
+			debug("warn srv: " + str(services_warning_unack))
+			set_light(color=orange, count=services_warning_unack)
+		    elif hosts_down_unack > 0:
+			debug("hosts down: " + str(hosts_down_unack))
+			set_light(color=blue, count=hosts_down_unack)
+		    elif services_unknown_unack > 0:
+			debug("services unknown: " + str(services_unknown_unack))
+			set_light(color=purple, count=services_unknown_unack)
+		    else:
+			debug("all is well")
+			if current_color != darkgreen:
+			    set_light(color=darkgreen)
 
             sleep(interval)
         except urllib2.URLError:
-            print "Error fetching info from %s, retrying in %d seconds." % ( url, interval )
+            print "Error fetching info from %s, retrying in %d seconds." % ( url, int(interval) )
             sleep(interval)
+
+	except KeyboardInterrupt:
+	    print "Cleaning up"
+	    if cylon_mode:
+		    cleanup()
 
     # end while
 
 
 if __name__ == "__main__":
+    f = open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                          "config.yaml"))
+    config = yaml.load(f)
+
+    url = config["icinga_baseurl"] + "/cgi-bin/tac.cgi?jsonoutput"
+    username = config["username"] or None
+    password = config["password"] or None
+    interval = int(config["interval"]) or 30
+    cylon_mode = config["enable_cylon"] or False
+
     print "In AD %d, icinga polling was beginning." % \
-        datetime.datetime.now().year
+        int(datetime.datetime.now().year)
     try:
         poll_icinga()
+    except KeyboardInterrupt:
+	print "Exiting gracefully, I hope"
+	if cylon_mode:
+		cleanup()
     except Exception as e:
-        print "Unexpected error:", sys.exc_info()[0], e
+        print "Unexpected error:", sys.exc_info()[0]
+	raise
